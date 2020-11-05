27a28d131bb8534f0b341e9607a7a07fe2b3f3f4
Anderson <anderson@pop-os.localdomain>
2020-11-04 21:29:33 -0500

    crud completo

--- app/Http/Livewire/PostComponent.php
+++ app/Http/Livewire/PostComponent.php
@@ -8,10 +8,70 @@ use Livewire\Component;
 
 class PostComponent extends Component
 {
+    public $name, $body, $post_id;
+
+    public $accion = 'store';
+
+    protected $rules = [
+        'name' => 'required|min:6',
+        'body' => 'required|min:10'
+    ];
+
+    protected $messages = [
+        'name.required' => 'nombre requerido',
+        'name.min' => 'minimo 6',
+        'body.required' => 'body requerido',
+        'body.min' => 'minimo 10',
+
+    ];
+
     public function render()
     {
-        $posts = Post::all();
+        $posts = Post::latest('id')->get();
 
         return view('livewire.post-component', compact('posts'));
+    }
+
+    public function store(){
+
+        $this->validate();
+
+        Post::create([
+
+            'name' => $this->name,
+            'body' => $this->body
+        ]);
+
+        $this->reset(['name', 'body']);
+    }
+
+    public function edit(Post $post){
+        $this->name = $post->name;
+        $this->body = $post->body;
+        $this->post_id = $post->id;
+
+        $this->accion = "update";
+
+    }
+
+    public function update(){
+        $post = Post::find($this->post_id);
+
+        $post->update([
+            'name' => $this->name,
+            'body' => $this->body
+        ]);
+
+        $this->reset(['name', 'body', 'accion', 'post_id']);
+
+    }
+
+    public function defaul(){
+        $this->reset(['name', 'body', 'accion', 'post_id']);
+
+    }
+
+    public function destroy(Post $post){
+        $post->delete();
     }
 }
--- resources/views/livewire/post-component.blade.php
+++ resources/views/livewire/post-component.blade.php
@@ -1,5 +1,36 @@
 <div class="container mx-auto">
-<table class="bg-white rounded-lg shadow overflow-hidden">
+
+       <div class="bg-white rounded-lg shadow overflow-hidden max-w-4xl mx-auto p-4 mb-6">
+            <div class="mb-3">
+
+                <label for="name" class="form-label mb-2">Nombre</label>
+                <input wire:model='name' placeholder="Ingrese un nombre"
+                class="form-control">
+                @error('name') <span class="error">{{ $message }}</span> @enderror
+
+            </div>
+
+            <div class="mb-3">
+                <label for="body" class="form-label mb-2">Descripción</label>
+                <textarea wire:model='body' cols="30" rows="4" class="form-control" placeholder="Ingrese la descripción"></textarea>
+                @error('body') <span class="error">{{ $message }}</span> @enderror
+
+            </div>
+            @if ($accion == 'store')
+
+            <button wire:click='store' class="btn primary">Crear Post</button>
+
+            @else
+
+            <button wire:click='update' class="btn primary">Actualizar</button>
+            <button wire:click='defaul' class="btn bg-red-500 hover:bg-red-700">Cancelar</button>
+
+            @endif
+
+    </div>
+
+
+<table class="bg-white rounded-lg shadow overflow-hidden max-w-4xl mx-auto">
     <thead class="bg-gray-50 border-b border-gray-200">
         <tr class="text-xs font-medium text-gray-500 uppercase text-left tracking-wider">
             <th class="px-6 py-3">ID</th>
@@ -9,15 +40,16 @@
 
         </tr>
     </thead>
-    <tbody>
+    <tbody class="divide-y divide-gray-200">
         @foreach ($posts as $post)
 
-        <tr>
-            <td>{{$post->id}}</td>
-            <td>{{$post->name}}</td>
-            <td>{{$post->body}}</td>
-            <td><button>Editar</button>
-            <button>Eliminar</button></td>
+        <tr class="text-sm text-gray-500">
+            <td class="px-6 py-4">{{$post->id}}</td>
+            <td class="px-6 py-4">{{$post->name}}</td>
+            <td class="px-6 py-4">{{$post->body}}</td>
+            <td class="px-6 py-4">
+                <button wire:click='edit({{$post}})' class="bg-blue-500 hover:bg-blue-700 mb-2 text-white px-4 py-2 rounded w-full">Editar</button>
+                <button wire:click='destroy({{$post}})' class="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded">Eliminar</button></td>
         </tr>
 
         @endforeach
